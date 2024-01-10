d7f9ec877caad9560a301e84e2d519f592b793b8
kw3082 <150286812+kw3082@users.noreply.github.com>
2024-01-11 00:59:32 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,59 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
+    let [글제목, 글제목변경] = useState(['남자코트추천', '하체운동종류', '저녁메뉴무엇?']);
+    let [따봉, 따봉변수] = useState([0, 0, 0]);
 
-  let [글제목, 글제목변경] = useState(['남자코트추천', '하체운동종류', '저녁메뉴무엇?'])
-  let [따봉, 따봉변수] = useState(0);
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>ReactBlog</h4>
-      </div>
-      
-      <div className="list">
-        <h4>{글제목[0]} <span onClick={() => {따봉변수(따봉+1)}}>🤞</span>{따봉}</h4>
-        <p>1월 10일 발행 </p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>1월 10일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>1월 10일 발행</p>
-      </div>
+    let [modal, setModal] = useState(false);
 
-      <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>ReactBlog</h4>
+            </div>
 
-      <Modal></Modal>
-
-    </div>
-  );
+            {글제목.map(function (a, i) {
+                return (
+                    <div className="list" key={i}>
+                        <h4
+                            onClick={() => {
+                                setModal(!modal);
+                            }}
+                        >
+                            {글제목[i]}{' '}
+                            <span
+                                onClick={() => {
+                                    let copy = [...따봉];
+                                    copy[i]++;
+                                    따봉변수(copy);
+                                }}
+                            >
+                                🤞
+                            </span>
+                            {따봉[i]}
+                        </h4>
+                        <p>1월 10일 발행</p>
+                    </div>
+                );
+            })}
+            {modal == true ? <Modal 글제목={글제목}></Modal> : null}
+        </div>
+    );
 }
 
-function Modal(){
-  return (
-    <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
-
-  )
+function Modal(props) {
+    return (
+        <div className="modal">
+            <h4>{props.글제목[0]}</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+            <button>글수정</button>
+        </div>
+    );
 }
 
 export default App;
